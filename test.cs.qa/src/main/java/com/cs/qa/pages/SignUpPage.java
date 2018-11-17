bb9b2ce66736bf13ef5bb2438520fc0414ee2d11
Varanasi <VaranaR@UTCCGL.com>
2018-11-17 20:40:30 +0530

    added test cases

--- test.cs.qa/src/main/java/com/cs/qa/pages/SignUpPage.java
+++ test.cs.qa/src/main/java/com/cs/qa/pages/SignUpPage.java
@@ -1,8 +1,13 @@
 package com.cs.qa.pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.cs.qa.base.TestBase;
 
@@ -20,16 +25,49 @@ public class SignUpPage extends TestBase {
 	@FindBy(xpath = "//button[contains(text(),'Sign up for GitHub')]")
 	WebElement signupButton;
 
+	@FindBy(xpath = "//div[contains(@class,'flash flash-error my-3')]")
+	WebElement mainError;
+
+	@FindBy(xpath = "//dd[contains(@class,'error')]")
+	WebElement erros;
+
 	public SignUpPage() {
 
 		PageFactory.initElements(driver, this);
 
 	}
 
-	public void SignUp() {
+	public String SignUp(String fMail, String fUname, String fPassword) throws InterruptedException {
 
-		userName.sendKeys("email@email.com");
+		userName.sendKeys(fUname);
+		email.sendKeys(fMail);
+		password.sendKeys(fPassword);
+
 		signupButton.click();
+		Thread.sleep(2000);
+		String Error = mainError.getText();
+		return Error;
+
+	}
+
+	public List pageValidation(String fMail, String fUname) {
+
+		userName.clear();
+		userName.sendKeys(fUname);
+		email.clear();
+		email.sendKeys(fMail);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebDriverWait wait = new WebDriverWait(driver, 3000);
+		wait.until(ExpectedConditions.visibilityOf(erros));
+		List erros = driver.findElements(By.xpath("//dd[contains(@class,'error')]"));
+		System.out.println(((WebElement) erros.get(0)).getText());
+		System.out.println(erros.size());
+		return erros;
 
 	}
 
--- test.cs.qa/src/test/java/com/cs/qa/tests/SignUpPageTests.java
+++ test.cs.qa/src/test/java/com/cs/qa/tests/SignUpPageTests.java
@@ -1,7 +1,10 @@
 package com.cs.qa.tests;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -27,10 +30,25 @@ public class SignUpPageTests extends TestBase {
 
 	}
 
-	@Test
-	public void test() {
-		signupPage.SignUp();
+	// @Test
+	public void test_invalid_username() throws InterruptedException {
+		String Error = signupPage.SignUp("test", "", "");
+		Assert.assertEquals(Error, "There were problems creating your account.");
 
+	}
+
+	@Test(priority = 1)
+	public void test_invalid_uname() {
+		// mail,username
+		List Uerror = signupPage.pageValidation("", "test");
+		Assert.assertEquals(((WebElement) Uerror.get(0)).getText(), "Username is already taken");
+	}
+
+	@Test(priority = 2)
+	public void test_invalid_email() throws InterruptedException {
+
+		List Uerror = signupPage.pageValidation("test@test.com", "");
+		Assert.assertEquals(((WebElement) Uerror.get(0)).getText(), "Email is invalid or already taken");
 	}
 
 	@AfterSuite
