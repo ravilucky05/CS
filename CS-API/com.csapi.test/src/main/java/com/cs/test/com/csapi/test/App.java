b4d5975d1cf87d4199a8c018f85e443f158f8ba2
Varanasi <VaranaR@UTCCGL.com>
2018-11-19 09:28:50 +0530

    created project structure

---
+++ CS-API/com.csapi.test/src/main/java/com/cs/test/com/csapi/test/App.java
@@ -0,0 +1,13 @@
+package com.cs.test.com.csapi.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
